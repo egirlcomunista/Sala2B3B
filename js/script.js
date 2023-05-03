5948df2ae96228dbf0f252b5c8508525ad753fe9
egirlcomunista <gasparyasmim07@gmail.com>
2023-05-03 10:51:48 -0400

    alterando icone menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function abrirMenu(){
     const divMenu = document.getElementById('respMenu').classList;
     divMenu.toggle('abrir');
+    const ic = document.getElementById('icone');
+    if(divMenu.contains('abrir'))
+    {
+      ic.innerHTML = 'close';
+    }
+    else
+    {
+      ic.innerHTML = 'menu';
+    }
+    
 }
